34af5082794a7464b6997de313f0d68d494a6301
Paul Keahi <paulkeahi@gmail.com>
2016-03-15 16:20:36 -1000

    working on subscribe still...php

--- sendmail.php
+++ sendmail.php
@@ -2,7 +2,7 @@
 // Email Submit
 // Note: filter_var() requires PHP >= 5.2.0
 if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
- 
+
   // detect & prevent header injections
   $test = "/(content-type|bcc:|cc:|to:)/i";
   foreach ( $_POST as $key => $val ) {
@@ -10,11 +10,11 @@ if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject'])
       exit;
     }
   }
-  
+
   //
-  mail( "spambox@designlab.co", $_POST['subject'], $_POST['message'], "From:" . $_POST['email'] );
- 
+  mail( "keahi@primespot.ninja", $_POST['subject'], $_POST['message'], "From:" . $_POST['email'] );
+
   //			^
-  //  Replace with your email 
+  //  Replace with your email
 }
 ?>
--- subscribe/subscribe.php
+++ subscribe/subscribe.php
@@ -1,9 +1,16 @@
 <?php
 // Note: filter_var() requires PHP >= 5.2.0
 if ( isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
-      
-      $e_mail = $_POST['email'] . "," . "\n";
-	  file_put_contents('email-list.txt', $e_mail, FILE_APPEND | LOCK_EX);
+  $to = "keahi@primespot.ninja";
+  $from = $_POST['email'];
+  $subject = "new subscriber";
+  $e_mail = $_POST['email'] . "," . "\n";
+  $message = "Your new suscriber is " + $e_mail;
+	$headers = "From:" . $from;
+  $headers2 = "From:" . $to;
+  mail($to,$subject,$message,$headers);
+  mail($from,$subject2,$message2,$headers2);
 
+  echo "Thank You for your Subscription";
 }
 ?>
